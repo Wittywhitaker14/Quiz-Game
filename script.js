46d1150a98d0bfd337265e84d39527e7443038f0
Austin Whitaker <wittywhitaker14@gmail.com>
2021-10-10 23:45:58 -0600

    Adding in the answer buttons and data tied to them when clicked

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // Setting up the variables
 var timerEl = document.getElementById('countdown')
 var startBtn = document.getElementById('start-btn')
+var nextBtn = document.getElementById('next-btn')
 var quiz = document.getElementById('quiz')
 var questionContainerEl = document.getElementById('question-container')
 var shuffleQuestions, currentQuestion
-
+var questionEl = document.getElementById('question')
+var answerEl = document.getElementById('answer-btns')
 
 startBtn.addEventListener('click', startQuiz)
 
 // The Functions
+
 
 function quizText() {
     quiz.classList.remove('hide')
@@ -32,28 +35,51 @@ function countdown() {
             timerEl.textContent = 'Timer: ' + timeLeft;
             timeLeft--;
         } else {
-            timerEl.textContent = '';
+            timerEl.textContent = 'Timer: 0';
             clearInterval(timeInterval);
         }
     }, 1000);
 }
 
 function nextQuestion () {
+    reset()
+    showQuestion(shuffleQuestions[currentQuestion])
+}
 
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answer.forEach(answer => {
+       var button = document.createElement('button') 
+       button.innerText = answer.text
+       button.classList.add('btn')
+       if (answer.correct) {
+           button.dataset.correct = answer.correct
+       }
+       button.addEventListener('click', answer)
+       answerEl.appendChild(button)
+    });
+}
+
+function reset() {
+    nextBtn.classList.remove('hide')
+    while (answerEl.firstChild){
+        answerEl.removeChild(answerEl.firstChild)
+    }
 }
 
 function answer () {
-
 }
 
 // The Questions
 
 var questions = [
     {
-        question: '',
+        question: 'Arrays in javascript could be used to store...',
         answer: [
-            {text:'', correct: true}, 
-            {text: '', correct: false}
+            {text: 'Numbers and Strings', correct: false},
+            {text: 'Booleans', correct: false},
+            {text: 'Other Arrays', correct: false},
+            {text: 'All the Above', correct: true} 
         ]
     }
 ]
